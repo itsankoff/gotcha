278f4e42ad960cfb5f4cdaf99364f08b0910b009
itsankoff <ivaylotsankov93@gmail.com>
2017-02-08 11:40:00 +0200

    Add super simple http and websocket server handlers

--- server/server.go
+++ server/server.go
@@ -1,16 +1,62 @@
 package server
 
 import (
-	"fmt"
+    "net/http"
+    "github.com/gorilla/websocket"
+    "log"
+    "fmt"
 )
 
 type Server struct {
+    upgrader        websocket.Upgrader
+    connections     []*websocket.Conn
+}
+
+func webSocketHandler(conn *websocket.Conn) {
+    log.Println("New websocket connection available")
+    for {
+        msgType, msg, err := conn.ReadMessage()
+        if err != nil {
+            log.Println("Connection read error", err.Error())
+            return
+        }
+
+        if msgType == websocket.TextMessage {
+            log.Println(string(msg))
+        }
+
+        if err = conn.WriteMessage(msgType, msg); err != nil {
+            log.Println("Connection write error", err.Error())
+            return
+        }
+    }
 }
 
 func New() *Server {
-	return &Server{}
+	return &Server{
+    }
 }
 
 func (s *Server) Start(host string) {
-	fmt.Println("Hello server")
+
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+        fmt.Fprintf(w, "Hello World")
+    })
+
+    var upgrader = websocket.Upgrader{
+        ReadBufferSize: 1024,
+        WriteBufferSize: 1024,
+    }
+
+    http.HandleFunc("/websocket", func(w http.ResponseWriter, r *http.Request) {
+        conn, err := upgrader.Upgrade(w, r, nil)
+        if err != nil {
+            log.Println(err)
+            return
+        }
+
+        go webSocketHandler(conn)
+    })
+
+    log.Fatal(http.ListenAndServe(":9000", nil))
 }
