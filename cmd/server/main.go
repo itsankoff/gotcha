2ce9a9b374519db51cc889a3473a550450a4daa9
itsankoff <ivaylotsankov93@gmail.com>
2017-02-13 12:46:56 +0200

    Add error handling for server's Start method

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -11,12 +11,15 @@ func main() {
     wss := server.NewWebSocket()
     s.AddTransport("127.0.0.1:9000", &wss)
     done := make(chan interface{})
+    go func() {
+        log.Println("Will close done channel")
+        time.Sleep(10 * time.Second)
+        log.Println("Close done channel")
+        close(done)
+    }()
 
-//     go func() {
-//         log.Println("Will close done channel")
-//         time.Sleep(10 * time.Second)
-//         log.Println("Close done channel")
-//         close(done)
-//     }()
-    s.Start(done)
+    err := s.Start(done)
+    if err != nil {
+        log.Fatal("Failed to start server")
+    }
 }
