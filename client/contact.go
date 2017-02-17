37ee593455e02b0c5a2fd75a69e56495f35d7270
itsankoff <ivaylotsankov93@gmail.com>
2017-02-17 19:13:25 +0200

    Remove unused type Contact

--- client/contact.go
+++
@@ -1,6 +0,0 @@
-package client
-
-type Contact struct {
-	Id   int64
-	Name string
-}
--- client/group.go
+++ client/group.go
@@ -2,5 +2,5 @@ package client
 
 type Group struct {
 	Id       int64
-	Contacts []*Contact
+	Contacts []string
 }
