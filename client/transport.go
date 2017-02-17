95908572d0e5d9164867b4b970a1fd3fea4e93d0
itsankoff <ivaylotsankov93@gmail.com>
2017-02-18 00:13:58 +0200

    Change client transport interface

--- client/transport.go
+++ client/transport.go
@@ -3,12 +3,12 @@ package client
 import "github.com/itsankoff/gotcha/common"
 
 type Transport interface {
-	Connect(host string) (bool, error)
+	Connect(host string) error
 	ConnectAsync(host string) chan bool
 	Disconnect()
-	Reconnect() (bool, error)
+	Reconnect() error
 	ReconnectAsync() chan bool
-	SendText(message string)
-	SendBinary(date []byte)
-	SetReceiver(<-chan common.Message)
+	SendText(message string) error
+	SendBinary(date []byte) error
+	SetReceiver(chan *common.Message)
 }
