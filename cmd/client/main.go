b80bea625abd7b6e5966d015105f2d7029f3873d
itsankoff <ivaylotsankov93@gmail.com>
2017-02-18 00:14:30 +0200

    Add simple show case of client usage

--- cmd/client/main.go
+++ cmd/client/main.go
@@ -2,9 +2,24 @@ package main
 
 import (
 	"github.com/itsankoff/gotcha/client"
+	"github.com/itsankoff/gotcha/common"
+	"log"
+	"time"
 )
 
 func main() {
-	c := client.New()
-	c.Connect("wss://127.0.0.1:9999")
+	input := make(chan *common.Message)
+	ws := client.NewWebSocketClient()
+	ws.SetReceiver(input)
+
+	c := client.New(ws)
+	err := c.Connect("ws://127.0.0.1:9000/websocket")
+	log.Println("connected", err)
+	userId, err := c.Register("pesho", "123")
+	log.Println("registered", err)
+
+	err = c.Authenticate(userId, "123")
+	log.Println("authenticated", err)
+
+	time.Sleep(10 * time.Second)
 }
