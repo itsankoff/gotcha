32fff76729bc9696eb90f5e709a30dd5e7c3040c
itsankoff <ivaylotsankov93@gmail.com>
2017-02-17 18:41:15 +0200

    Make History type a message handler for get_history

--- server/history.go
+++ server/history.go
@@ -7,10 +7,62 @@ import (
 )
 
 type History struct {
+	input       chan *common.Message
+	outputStore *OutputStore
 }
 
-func NewHistory() *History {
-	return &History{}
+func NewHistory(input chan *common.Message, outputStore *OutputStore) *History {
+	history := &History{
+		input:       input,
+		outputStore: outputStore,
+	}
+
+	go history.listen()
+	return history
+}
+
+func (h History) listen() {
+	for {
+		select {
+		case msg := <-h.input:
+			valid := h.validate(msg)
+			if !valid {
+				log.Println("Failed to validate history message")
+				response := common.NewResponse(msg, "invalid message")
+				h.outputStore.Send(response)
+				return
+			}
+
+			payload, err := msg.ParseJsonData()
+			if err != nil {
+				log.Printf("Failed to parse history message from %s", msg.From())
+				response := common.NewResponse(msg, "bad message")
+				h.outputStore.Send(response)
+				return
+			}
+
+			cmd := msg.Cmd()
+			switch cmd {
+			case "get_history":
+				userId := msg.From()
+				forUserId := payload["for_user_id"].(string)
+				fromDate := time.Unix(int64(payload["from_date"].(float64)), 0)
+				toDate := time.Unix(int64(payload["to_date"].(float64)), 0)
+				messages := h.GetHistory(userId, forUserId, fromDate, toDate)
+				accumulated := h.accumulate(messages)
+				response := common.NewResponse(msg, accumulated)
+				h.outputStore.Send(response)
+			}
+		}
+	}
+}
+
+func (h History) validate(msg *common.Message) bool {
+	return true
+}
+
+func (h History) accumulate(messages []*common.Message) string {
+	return ""
 }
 
 func (h History) AddMessage(msg *common.Message) bool {
@@ -18,8 +70,7 @@ func (h History) AddMessage(msg *common.Message) bool {
 	return false
 }
 
-func (h History) GetHistory(user *common.User,
-	from time.Time,
-	to time.Time) []*common.Message {
+func (h History) GetHistory(userId string, forUserId string,
+	from time.Time, to time.Time) []*common.Message {
 	return []*common.Message{}
 }
