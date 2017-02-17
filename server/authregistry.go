dd69326c6537c453c4872f82217778d1484663e7
itsankoff <ivaylotsankov93@gmail.com>
2017-02-17 16:08:41 +0200

    * Add dummy auth registry

---
+++ server/authregistry.go
@@ -0,0 +1,17 @@
+package server
+
+type AuthRegistry struct {
+
+}
+
+func NewAuthRegistry() *AuthRegistry {
+    return &AuthRegistry{}
+}
+
+func (a *AuthRegistry) Register(username string, pass string) (string, bool) {
+    return "pesho", true
+}
+
+func (a *AuthRegistry) Authenticate(userId string, pass string) bool {
+    return true
+}
