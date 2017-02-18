e3a2c70157b7e423ce8c35ca714381e7b0c73008
itsankoff <ivaylotsankov93@gmail.com>
2017-02-18 05:28:32 +0200

    Implement inmemory contact store

--- server/contactstore.go
+++ server/contactstore.go
@@ -1,24 +1,50 @@
 package server
 
+import "log"
+
 type ContactStore struct {
+	contacts map[string][]string
 }
 
 func NewContactStore() *ContactStore {
-	return &ContactStore{}
+	return &ContactStore{
+		contacts: make(map[string][]string),
+	}
 }
 
 func (c *ContactStore) AddContact(userId string, contactId string) bool {
+	userContacts, ok := c.contacts[userId]
+	if !ok {
+		c.contacts[userId] = []string{}
+		userContacts = c.contacts[userId]
+	}
+
+	userContacts = append(userContacts, contactId)
 	return true
 }
 
 func (c *ContactStore) RemoveContact(userId string, contactId string) bool {
-	return true
+	userContacts, ok := c.contacts[userId]
+	if !ok {
+		log.Println("No contacts for user", userId)
+		return false
+	}
+
+	for i, uc := range userContacts {
+		if uc == contactId {
+			userContacts = append(userContacts[:i], userContacts[i+1:]...)
+			return true
+		}
+	}
+
+	return false
 }
 
 func (c *ContactStore) ListContacts(userId string) ([]string, bool) {
-	return []string{}, true
-}
+	userContacts, ok := c.contacts[userId]
+	if !ok {
+		return []string{}, false
+	}
 
-func (c *ContactStore) SearchContact(userId string, contactName string) string {
-	return ""
+	return userContacts, true
 }
