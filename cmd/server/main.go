e4dbfeb8d773865f87e97607a453d3e6324def53
itsankoff <ivaylotsankov93@gmail.com>
2017-02-18 05:07:47 +0200

    Add flag params for server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,17 +1,31 @@
 package main
 
 import (
+	"flag"
 	"github.com/itsankoff/gotcha/server"
 	"log"
 )
 
 func main() {
-	s := server.New()
+	config := server.NewConfig()
+	config.ListenHost = *flag.String("host", ":9000", "host to listen")
+	config.FileServerHost = *flag.String("file_host", ":9000", "host to server files")
+	config.FileServerPath = *flag.String("file_path", "/files", "query file path to access files")
+	config.FileServerFolder = *flag.String("file_folder", "./", "storage folder")
+	flag.Parse()
+
+	args := flag.Args()
+	if len(args) > 0 && args[0] == "--help" {
+		flag.PrintDefaults()
+		return
+	}
+
+	srv := server.New(config)
 	wss := server.NewWebSocket()
-	s.AddTransport("127.0.0.1:9000", &wss)
+	srv.AddTransport("127.0.0.1:9000", &wss)
 	done := make(chan interface{})
 
-	err := s.Start(done)
+	err := srv.Start(done)
 	if err != nil {
 		log.Fatal("Failed to start server")
 	}
